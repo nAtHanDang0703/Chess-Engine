613a06d1b5965a913e770c76758201f800275b1b
nAtHanDang0703 <77898991+nAtHanDang0703@users.noreply.github.com>
2022-11-01 00:29:10 -0400

    Add files via upload

---
+++ ChessPieces/ChessMain.py
@@ -0,0 +1,105 @@
+"""
+This is our main driver file. It will be responsible for handling user input and displaying the current GameState objects.
+"""
+
+import pygame as p
+from ChessPieces import ChessEngine
+
+WIDTH = HEIGHT = 512 #400 is another good option
+DIMENSION = 8 #dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #for animations later on
+IMAGES = {}
+
+'''
+initialize a global dictionary of images. This will be called exactly once in the main
+'''
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"),(SQ_SIZE,SQ_SIZE))
+    #Note: we can access an image by saying 'IMAGES['wP']'
+
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False #flag variable for when a move is made
+
+    loadImages() #only do this once, before the while loop
+    running = True
+    sqSelected = () #no square selected, keep track of the last clisk of the user (tuple: (row, column))
+    playerClicks  = [] #keep track of player clicks (two tuples: [(6,4), (4,4)])
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            #mouse handler
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x, y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): #the user clicked the same square twice
+                    sqSelected = () #deselect
+                    playerClicks = [] #clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) #append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: #after 2nd click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                    sqSelected = () #reset user clicks
+                    playerClicks = []
+            #key handlers
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: #undo when 'z' is pressed
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Responsible for all the graphics within a current game state.
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw squares on the board
+    #add in piece highlighting or move suggestions (later)
+    drawPieces(screen, gs.board) #draw pieces on top of those squares
+
+'''
+Draw squares on the board. The top left square is always light.
+'''
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not empty square
+                screen.blit(IMAGES[piece],p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+if __name__ == "__main__":
+    main()
